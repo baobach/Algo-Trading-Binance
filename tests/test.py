1f291d6942c947b84fb5ec6357bc4cbb4d9803d9
baobach <95854748+baobach@users.noreply.github.com>
2024-01-28 14:21:56 +0700

    Testing tickcomp.py package

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import numpy as np
 import pytest
+import sys
+import os
+
+# Add the project directory to the Python path
+project_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(project_dir, '..'))
+
+# Now import the TickBar class
 from tickcomp import TickBar
+
 
 # Sample DataFrame fixture
 @pytest.fixture
@@ -9,8 +18,8 @@ def sample_dataframe():
     data = {
         'timestamp': pd.date_range(start='2022-01-01', periods=100, freq='T'),
         'close': np.random.rand(100),
-        'volume': np.random.randint(100, 1000, size=100),
-        'dollar_volume': np.random.uniform(10, 100, size=100)
+        'v': np.random.randint(100, 1000, size=100),
+        'dv': np.random.uniform(10, 100, size=100)
     }
     df = pd.DataFrame(data)
     df.set_index('timestamp', inplace=True)
@@ -27,13 +36,13 @@ def test_price_tick(sample_dataframe):
 def test_volume_tick(sample_dataframe):
     tick_bar = TickBar(sample_dataframe)
     target_volume = 500
-    resampled_volume = tick_bar.volume_tick('volume', target_volume)
+    resampled_volume = tick_bar.volume_tick(target_volume)
     assert isinstance(resampled_volume, pd.DataFrame)
 
 # Test dollar_tick method
 def test_dollar_tick(sample_dataframe):
     tick_bar = TickBar(sample_dataframe)
     target_dollar = 50
-    resampled_dollar = tick_bar.dollar_tick('dollar_volume', target_dollar)
+    resampled_dollar = tick_bar.dollar_tick(target_dollar)
     assert isinstance(resampled_dollar, pd.DataFrame)
 
